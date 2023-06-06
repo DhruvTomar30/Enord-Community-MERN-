65217cbedf7d4ac4bac079e9ab604e2a9ebf8898
Akshat <iamakku0.0.0.1@gmail.com>
2023-06-06 22:39:18 +0530

    changes in side bar position is fixed and shifted to left by 4cm

--- src/App.js
+++ src/App.js
@@ -11,21 +11,23 @@ import ViewQuestion from './components/ViewQuestion';
 import Question from './components/Add-Question/Question';
 import Auth from './components/Auth'
 
+
 function App() {
 return (
     <div className="App">   
       {/* <p>Hello</p> */}
       <Router>
         <Header />
+
         <Switch>
           
           <Route exact path="/auth" component={Auth  } />
           <Route exact path="/add-question" component={Question  } />
           <Route exact path="/question" component={ViewQuestion } />
           <Route exact path="/" component={Enord} />
-
           
         </Switch>
+
       </Router>
     </div>
   );
