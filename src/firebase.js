6cdc5455994dfb6b9f557b7821010a5a26d45b90
dhruvtomar2020@gmail.com <Dhruv Tomar>
2023-06-08 16:40:44 +0530

    Authentication done.

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth,GoogleAuthProvider} from 'firebase/auth'
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8PARrRlEEfuX69LkZNxn9pe-FNWQHnq8",
-  authDomain: "enordcommunity.firebaseapp.com",
-  projectId: "enordcommunity",
-  storageBucket: "enordcommunity.appspot.com",
-  messagingSenderId: "715937694775",
-  appId: "1:715937694775:web:1b792ce0d0b52b61a06bb2",
-  measurementId: "G-WGV9SBL5ZP"
+  apiKey: "AIzaSyBdtndaIJhjcewnFLebYgRlvsuKex6wqxo",
+  authDomain: "community-website-a33fd.firebaseapp.com",
+  projectId: "community-website-a33fd",
+  storageBucket: "community-website-a33fd.appspot.com",
+  messagingSenderId: "841583262549",
+  appId: "1:841583262549:web:b750177bb9c8f818f7cddc",
+  measurementId: "G-40DK7K8RBE"
 };
 
 // Initialize Firebase
